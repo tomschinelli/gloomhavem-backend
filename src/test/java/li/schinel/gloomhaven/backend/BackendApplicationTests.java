6d2797c7ab199f0d6b43720dfb2e02c7c80bb493
tomschinelli <tom.schinelli@gmail.com>
2023-03-22 21:18:33 +0100

    Basic-Setup: Add failing test

--- src/test/java/li/schinel/gloomhaven/backend/BackendApplicationTests.java
+++ src/test/java/li/schinel/gloomhaven/backend/BackendApplicationTests.java
@@ -1,11 +1,17 @@
 package li.schinel.gloomhaven.backend;
 
+import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class BackendApplicationTests implements WithAssertions {
 
     @Test
     void contextLoads() {}
+
+    @Test
+    void testFailure() {
+        assertThat(true).isFalse();
+    }
 }
