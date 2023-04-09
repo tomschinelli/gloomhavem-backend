131e8d2e83491cc33bb08cf9d0a50eb42ca42ed6
tom.schinelli <tom.schinelli@gmail.com>
2023-04-09 13:31:40 +0200

    Add tescontainers for db testing

--- src/test/java/li/schinel/gloomhaven/backend/BackendApplicationTests.java
+++ src/test/java/li/schinel/gloomhaven/backend/BackendApplicationTests.java
@@ -1,17 +1,17 @@
 package li.schinel.gloomhaven.backend;
 
-import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-class BackendApplicationTests implements WithAssertions {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// @SpringBootTest()
+@ActiveProfiles("test")
+class BackendApplicationTests {
 
     @Test
     void contextLoads() {}
 
-    @Test
-    void testFailure() {
-        assertThat(true).isTrue();
+    {
     }
 }
